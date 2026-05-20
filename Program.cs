bb5bca3591d6630804fac20ae4df8bb09e74a195
Yangshan-Liu55 <yangshan.liu55@gmail.com>
2026-05-20 16:22:23 +0200

    Ange personals info och utskriva registret i en konsol.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Text;
+
+string? numStr;
+Console.Write("Ange antalet anställda: ");
+numStr = Console.ReadLine();
+if (numStr == null)
+{
+    return;
+}
+
+int num = int.Parse(numStr);
+StringBuilder output = new StringBuilder();
+ArrayList personnelList = new ArrayList();
+
+for (int i = 1; i <= num; i++)
+{
+    string? name;
+    Console.Write("Ange namn för personalnummer " + i + ": ");
+    name = Console.ReadLine();
+    string? salary;
+    Console.Write("Ange lön för personalnummer " + i + ": ");
+    salary = Console.ReadLine();
+    Personnel per = new Personnel();
+    per.Name = name;
+    per.Salary = salary;
+
+    personnelList.Add(per);
+}
+
+output.AppendLine("Personallista:");
+
+foreach (Personnel per in personnelList)
+{
+    output.AppendLine(per.Name + " " + per.Salary);
+}
+
+Console.Write(output.ToString());
+
+class Personnel()
+{
+    public string? Name { get; set;}
+    public string? Salary { get; set;}
+}
